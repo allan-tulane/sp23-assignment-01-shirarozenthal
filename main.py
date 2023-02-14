12bc4f65f7d63f078a17c25936be8e7347b5ff74
shirarozenthal <112002091+shirarozenthal@users.noreply.github.com>
2023-02-13 20:09:53 -0600

    Updated Question 2a

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ See assignment-01.pdf for details.
 # no imports needed.
 
 def foo(x):
-    ### TODO
-    pass
+    
+    if x < 1:
+        return x
+    
+    else:
+        (ra, rb) = (foo(x - 1), foo(x - 2))
+        return ra + rb
+    
+   
 
 def longest_run(mylist, key):
     ### TODO
